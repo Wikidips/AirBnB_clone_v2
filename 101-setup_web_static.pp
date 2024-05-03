8da030f4f94edd531ed4f532cd674fb6b0078b55
Wikidips <wikidips18@gmail.com>
2024-05-03 23:28:22 +0100

    come true

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,5 +1,6 @@
 # Configures a web server for deployment of web_static.
 
+# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -26,44 +27,64 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-}
+} ->
 
-file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared']:
-  ensure => 'directory',
-}
+file { '/data':
+  ensure  => 'directory'
+} ->
+
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "Holberton School Puppet\n"
-}
+} ->
 
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-}
+} ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-file { ['/var/www', '/var/www/html']:
-  ensure => 'directory',
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Holberton School Nginx\n"
-}
+} ->
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-}
+} ->
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-}
+} ->
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
