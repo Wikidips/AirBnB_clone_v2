0c6436fe2651c7fc5fbca4b402e66de8a4dc6ad4
Wikidips <wikidips18@gmail.com>
2024-05-03 22:34:02 +0100

    hent

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-# Sets up web servers for the deployment of web_static
+# This script sets up web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
-    <p>Holberton School</p>
+    Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
@@ -19,6 +19,6 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 nginx_config="/etc/nginx/sites-available/default"
-sudo sed -i '/^\s*server_name _;/a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' $nginx_config
+sudo sed -i '/^\s*server_name _;/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' $nginx_config
 
 sudo service nginx restart
