bdb1d319ba44951bec389be6cbfd5b37b767412e
Wikidips <wikidips18@gmail.com>
2024-05-03 22:41:50 +0100

    gjh

---
+++ 1-pack_web_static.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """Generates an archive from the contents of the 'web_static' folder."""
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/".format(filename))
+        return "versions/web_static_{}.tgz".format(filename)
+    except Exception:
+        return None
